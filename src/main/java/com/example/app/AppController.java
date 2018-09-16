75a4a1cc7b03b9b0d41a1b7e0c6e33e5d1bec030
ThomasTong <137537069@qq.com>
2018-09-16 22:53:49 +0800

    Add config server and client.

--- src/main/java/com/example/app/AppController.java
+++ src/main/java/com/example/app/AppController.java
@@ -1,5 +1,7 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,4 +18,17 @@ public class AppController {
         return "Greetings from Spring Boottest!";
     }
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:de}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
 }
